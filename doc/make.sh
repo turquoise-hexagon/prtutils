a9425a4b81fc9701ccb16b2c3e54aea63799a565
turquoise-hexagon <camille@lzr.pw>
2020-03-06 19:28:46 +0100

    improve build script for man pages

--- doc/make.sh
+++ doc/make.sh
@@ -1,19 +1,43 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 cd "${0%/*}"
 
 # check for rst2man
-type rst2man.py > /dev/null 2>&1 || {
+type rst2man.py &> /dev/null || {
     echo 'error : install docutils first' >&2
     exit 1
 }
 
-rm -rf   man
+declare -A array
+
+# build list of files to convert
+for rst in rst/*.rst; {
+    man=${rst##*/}
+    man=man/${man%.rst}.1.gz
+
+    [[ -e $man ]] && {
+        {
+            read -r a
+            read -r b
+        } < \
+            <(stat -c '%Y' -- "$rst" "$man")
+
+        ((a > b)) || continue
+    }
+
+    array[$rst]=$man
+}
+
 mkdir -p man
 
-for rst in rst/*.rst; do
-    man=${rst##*/}
-    man=man/${man%rst}1.gz
+{
+    set -x
 
-    rst2man.py "$rst" | gzip > "$man"
-done
+    for rst in "${!array[@]}"; {
+        rst2man.py "$rst" | gzip > "${array[$rst]}"
+    }
+} |&
+    while IFS= read -r line; do
+        [[ $line =~ '+ 'for ]] ||
+            printf '%s\n' "$line"
+    done
